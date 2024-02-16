3084cddbd2fa81e9a54d09a96ac33483e75855ff
Maxim <nikitin.maxim.99@gmail.com>
2024-02-16 13:38:25 +0300

    decmpose files

--- Js/burger-menu.js
+++ Js/burger-menu.js
@@ -1,16 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const burgerIcon = document.querySelector('.navbar-toggler');
+    const menuList = document.getElementById('menu-list');
+    const close_modal = document.querySelector('.close_modal');
 
-
-
-    document.addEventListener('DOMContentLoaded', function () {
-        const burgerIcon = document.querySelector('.navbar-toggler');
-        const menuList = document.getElementById('menu-list');
-        const close_modal = document.querySelector('.close_modal');
-
-        burgerIcon.addEventListener('click', function () {
-            // Переключаем положение меню
-            menuList.style.left = (menuList.style.left === '0%') ? '-100%' : '0%';
-        });
-        close_modal.addEventListener('click', function () {
-            menuList.style.left = (menuList.style.left === '100%') ? '100%' : '-100%';
-        })
+    burgerIcon.addEventListener('click', function () {
+        // Переключаем положение меню
+        menuList.style.left = (menuList.style.left === '0%') ? '-100%' : '0%';
     });
+    close_modal.addEventListener('click', function () {
+        menuList.style.left = (menuList.style.left === '100%') ? '100%' : '-100%';
+    })
+});
